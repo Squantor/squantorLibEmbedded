2b2ee8f338ac9fbcd85a2da8b8fe3f33fdd78260
Squantor <squantmuts@gmail.com>
2020-08-14 19:39:48 +0200

    added size function to array

---
+++ inc/array.hpp
@@ -0,0 +1,41 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2020-2020 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/** 
+ * \file array.hpp
+ * 
+ * definition of the array datatype
+ * 
+ * Definition of the array datatype
+ */ 
+#ifndef ARRAY_HPP
+#define ARRAY_HPP
+
+#include <cstddef>
+
+namespace util
+{
+    namespace detail
+    {
+    }
+    /**
+     * \brief Standard array type modeled after std::array
+     * 
+     * Standard array modeled after the C++ std::array type.
+     * 
+     * \param[in] T typename
+     * \param[in] N size of array
+     */
+    template<typename T, size_t N>
+    struct array 
+    {
+        size_t size() { return N; }
+
+        T __data[N]; /** Data store used internally */
+    };
+}
+
+#endif
---
+++ libEmbedded.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2020-2020 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
+
+
+# libEmbedded library settings
+#
+# Version: 20200814
+
+# library files and includes
+INCLUDES += -IlibEmbedded/inc
+
