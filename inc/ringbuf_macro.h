063bf3492717ce24034ebcb88826fed8b4c8b927
Squantor <squantmuts@gmail.com>
2021-02-01 20:41:43 +0100

    initial rename

--- inc/ringbuf_macro.h
+++ inc/ringbuf_macro.h
@@ -1,34 +1,23 @@
 /*
-The MIT License (MIT)
-
-Copyright (c) 2015 Anton Bilos
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-/*
-queue instantiation macro's used by the primary user of the queue.
-Restrictions:
-only use direct types that can be copied by assignment.
-*/
-
-#ifndef __QUEUE_TEMPLATE_H__
-#define __QUEUE_TEMPLATE_H__
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2021 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/** 
+ * \file ringbuf_macro.c
+ * 
+ * Macro based ringbuffer implementation, instantiate in the file you want to use 
+ * the three macros:
+ * TEMPLATE_QUEUE_VARS(name, type, size)
+ * TEMPLATE_QUEUE_PROTO(name, type) // generates prototype definitions
+ * TEMPLATE_QUEUE_FUNCTIONS(name, type, size) // generates the actual code
+ * 
+ * This macro based ringbuffer is a poormans template metaprogramming
+ * 
+ */ 
+#ifndef RINGBUFFER_MACRO_H
+#define RINGBUFFER_MACRO_H
 
 #ifdef __cplusplus
 extern "C" {
