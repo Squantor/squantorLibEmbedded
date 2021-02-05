c7d3bbcb721402e9d7540dc74c4f6287c242a0cc
Squantor <squantmuts@gmail.com>
2021-02-05 10:25:30 +0100

    incorporated review comments from zhmu

--- src/parse/parsedigit.c
+++ src/parse/parsedigit.c
@@ -1,41 +1,41 @@
 /*
-MIT License
-
-Copyright (c) 2019 Bart Bilos
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2021 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/** 
+ * \file parsedigit.c
+ * 
+ */ 
 #include <parsedigit.h>
 #include <ctype.h>
 
-const char digitsLower[16] = "0123456789abcdef";
-const char digitsHigher[16] = "0123456789ABCDEF";
 
-parseResult parseDigit(const char c, unsigned int *value)
+/**
+ * \brief Parse an ASCII character and translate it it into its decimal value.
+ * 
+ * \param c     ASCII character to translate
+ * \param value pointer to an int that will contained the value of the character
+ * 
+ * \return true for successful translation
+ */
+bool parseDigit(const char c, unsigned int *value)
 {
-    for(int i = 0; i < 16; i++)
+    if (c >= '0' && c <= '9') 
     {
-        if(digitsLower[i] == c || digitsHigher[i] == c)
-        {
-            *value = (unsigned int) i;
-            return parseOk;
-        }
+        *value = c - '0'; 
+        return true;
     }
-    return parseFailed;
+    if (c >= 'a' && c <= 'f') 
+    {
+        *value = c - 'a'; 
+        return true;
+    }
+    if (c >= 'A' && c <= 'F') 
+    {
+        *value = c - 'A';
+        return true;
+    }
+    return false;
 }
