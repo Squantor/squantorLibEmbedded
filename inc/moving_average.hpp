49eaf2e3609ae30edecef02d13dda8272c97c630
Squantor <squantmuts@gmail.com>
2021-02-09 21:58:53 +0100

    added empty header for C++ moving average filter

---
+++ inc/moving_average.hpp
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2021 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/** 
+ *\file moving_average.hpp
+ * 
+ * Implements a moving average filter
+ * 
+ */ 
+#ifndef MOVING_AVERAGE_HPP
+#define MOVING_AVERAGE_HPP
+
+#endif
