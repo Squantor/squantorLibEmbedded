00e47777a748f8a08f677dfb504c421208619184
Squantor <squantmuts@gmail.com>
2022-01-25 18:47:36 +0100

    made bitzoom bit more compact

--- src/bit/bitzoom.c
+++ src/bit/bitzoom.c
@@ -10,50 +10,19 @@
  * Contains the bitZoom function
  *
  */
-
+#include <stddef.h>
 #include <bit.h>
 
 uint16_t bitZoom(uint8_t byte) {
   uint16_t input = byte;
   uint16_t mask = 0x0001;
   uint16_t output = input & mask;
-  input = input << 1;
-  mask = mask << 1;
-  output = output | (input & mask);
-  mask = mask << 1;
-  output = output | (input & mask);
-  input = input << 1;
-  mask = mask << 1;
-  output = output | (input & mask);
-  mask = mask << 1;
-  output = output | (input & mask);
-  input = input << 1;
-  mask = mask << 1;
-  output = output | (input & mask);
-  mask = mask << 1;
-  output = output | (input & mask);
-  input = input << 1;
-  mask = mask << 1;
-  output = output | (input & mask);
-  mask = mask << 1;
-  output = output | (input & mask);
-  input = input << 1;
-  mask = mask << 1;
-  output = output | (input & mask);
-  mask = mask << 1;
-  output = output | (input & mask);
-  input = input << 1;
-  mask = mask << 1;
-  output = output | (input & mask);
-  mask = mask << 1;
-  output = output | (input & mask);
-  input = input << 1;
-  mask = mask << 1;
-  output = output | (input & mask);
-  mask = mask << 1;
-  output = output | (input & mask);
-  input = input << 1;
-  mask = mask << 1;
-  output = output | (input & mask);
+  for (size_t i = 0; i < 8; i++) {
+    input = input << 1;
+    mask = mask << 1;
+    output = output | (input & mask);
+    mask = mask << 1;
+    output = output | (input & mask);
+  }
   return output;
 }
