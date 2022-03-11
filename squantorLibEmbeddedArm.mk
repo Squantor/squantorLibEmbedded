4480e6b5cc9d7e3624507ebf9ebcdc3b51c9b1ec
Squantor <squantmuts@gmail.com>
2022-03-11 16:14:21 +0100

    makefile for arm specific things

---
+++ squantorLibEmbeddedArm.mk
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
+
+
+# squantorLibEmbedded library settings
+# Arm specific files
+#
+# Version: 20200814
+
+# library files and includes, assuming you also add squantorLibEmbedded
+
+FILES += 
+
