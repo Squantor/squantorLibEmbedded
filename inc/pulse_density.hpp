2847059e050376b027033e6c1392ed4380e6ce3c
Squantor <squantmuts@gmail.com>
2022-03-13 14:01:50 +0100

    implemented C++ pulse density modulator

--- inc/pulse_density.hpp
+++ inc/pulse_density.hpp
@@ -11,4 +11,26 @@
 #ifndef PULSE_DENSITY_HPP
 #define PULSE_DENSITY_HPP
 
+#include <stdint.h>
+
+namespace util {
+class PulseDensityModulator {
+ public:
+  PulseDensityModulator(uint32_t threshold, uint32_t step) : sum(0), threshold(threshold), step(step) {}
+
+  bool output() {
+    sum += step;
+    if (sum > threshold) {
+      sum -= threshold;
+      return true;
+    } else {
+      return false;
+    }
+  }
+  uint32_t sum;
+  uint32_t threshold;
+  uint32_t step;
+};
+}  // namespace util
+
 #endif
