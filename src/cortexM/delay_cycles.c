f1a82cc06ef5104df21c87e195ad3f38e89f3249
Squantor <squantmuts@gmail.com>
2022-03-15 14:51:39 +0100

    added attributation

--- src/cortexM/delay_cycles.c
+++ src/cortexM/delay_cycles.c
@@ -4,6 +4,11 @@
  * Copyright (c) 2022 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
+/* \brief cycle accurate delay loop
+ * Cycle accurate delay loop for Cortex M processors, taken from this forum post by
+ * Alex Taradov on the eevblog forum on arm NOP discussion:
+ * https://www.eevblog.com/forum/microcontrollers/arm-nop/msg3935962/#msg3935962
+ */
 #include <delay_cycles.h>
 
 __attribute__((noinline, section(".ramfunc"))) void delay_cycles(uint32_t cycles) {
