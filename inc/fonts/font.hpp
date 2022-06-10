b8eccbc41194b057f7f5a1df817a7564b6d93c00
Squantor <squantmuts@gmail.com>
2022-06-10 14:26:45 +0200

    started with font info structure

---
+++ inc/fonts/font.hpp
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/*! \file font.h
+ *  \brief definitions and code commonly used for monospaced fonts
+ *
+ */
+#ifndef FONT_H
+#define FONT_H
+
+#include <stdint.h>
+
+struct font {
+  const uint16_t xSize;
+  const uint16_t ySize;
+  const std::byte *fontBitmap;
+  const uint16_t *ascii2index;
+};
+
+#endif
