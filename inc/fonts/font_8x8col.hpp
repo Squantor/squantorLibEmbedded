fbf302ec3f5fd7db6f2a2e1b1e44483cbb8c54d6
Squantor <squantmuts@gmail.com>
2022-06-10 14:41:32 +0200

    WIP font definitions

---
+++ inc/fonts/font_8x8col.hpp
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/*! \file font_8x8col.hpp
+ *  \brief definition of 8 by 8 column oriented font
+ *
+ */
+#ifndef FONT_8X8COL_H
+#define FONT_8X8COL_H
+
+#include <fonts/font.hpp>
+
+extern font mono8x8col;
+
+#endif
---
+++ src/font/font_8x8col.cpp
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/*! \file font_8x8col.cpp
+ *  \brief data for 8 by 8 column oriented font
+ *
+ */
+#include <fonts/font_8x8col.hpp>
+
+font mono8x8col{8,8,};
+
+
+
