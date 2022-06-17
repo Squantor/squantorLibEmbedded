ee6701d8c34484492a061327f977774ae4a6bf1b
Squantor <squantmuts@gmail.com>
2022-06-17 17:11:53 +0200

    removed TODO: aligned writes use faster path

--- src/bit/bitblit1d.cpp
+++ src/bit/bitblit1d.cpp
@@ -16,8 +16,6 @@
 namespace util {
 
 // WIP change to zero checks and remove indexes
-// TODO: move array aligned access, a memcpy is quicker and RMW is redundant for it
-// but do not forget the last dangling bits in that case!
 void bitblit1d(uint8_t *dest, size_t destSize, unsigned int destPos, uint8_t *src, unsigned int srcSize) {
   // compute indexes and limits
   bool alignedWrites = (destPos & 7) == 0;
@@ -41,27 +39,28 @@ void bitblit1d(uint8_t *dest, size_t destSize, unsigned int destPos, uint8_t *sr
     *dest = data | ((*src << destBit) & mask);
     return;
   }
-  // first element
-  data = *dest & ~mask;
-  *dest = data | (*src << destBit);
-  destIndex++;
-  dest++;
-  // continue
-  while (destIndex != destIndexEnd) {
-    if (!alignedWrites)  // array aligned access, this is not needed
-    {
-      data = *dest & mask;
-      *dest = data | (*src >> (8 - destBit));
-    }
-    src++;
+  if (alignedWrites) {
+    memcpy(dest, src, count);
+    dest = dest + count;
+  } else {
+    // first element
     data = *dest & ~mask;
     *dest = data | (*src << destBit);
     destIndex++;
     dest++;
-    count--;
+    // continue
+    while (destIndex != destIndexEnd) {
+      data = *dest & mask;
+      *dest = data | (*src >> (8 - destBit));
+      src++;
+      data = *dest & ~mask;
+      *dest = data | (*src << destBit);
+      destIndex++;
+      dest++;
+      count--;
+    }
   }
   // last element
-
   if ((destIndexEnd != destSize)) {  // check if the end operation would write over boundary
     if (alignedWrites) {             // if we had aligned access, then we need next srcIndex
       src++;
