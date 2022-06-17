f0ea5cde05db0124de9e9dd66236946bcfd4a00d
Squantor <squantmuts@gmail.com>
2022-06-17 16:44:47 +0200

    removed WIP change to pointer math

--- src/bit/bitblit1d.cpp
+++ src/bit/bitblit1d.cpp
@@ -14,9 +14,7 @@
 
 namespace util {
 
-// WIP change endbit bounds
-// TODO change from indices to pointer math
-// TODO change to zero checks
+// TODO change to zero checks and remove indexes
 // TODO we could make aligned access a flag instead of a special mask?
 // TODO: extract array aligned access, RMW is redundant for it
 // but do not forget the last dangling bits in that case!
@@ -24,6 +22,7 @@ void bitblit1d(uint8_t *dest, size_t destSize, unsigned int destPos, uint8_t *sr
   // compute indexes and limits
   unsigned int srcIndex = 0;
   unsigned int destIndex = destPos / 8;
+  dest = dest + destPos / 8;
   unsigned int destBit = destPos & 7;
   unsigned int destOffset = destPos == 0 ? 0 : 1;
   unsigned int destIndexEnd = (destPos + srcSize + destOffset) / 8;
@@ -35,36 +34,42 @@ void bitblit1d(uint8_t *dest, size_t destSize, unsigned int destPos, uint8_t *sr
   // case for less then element bits write within a single element
   if (srcSize < 8 && endBit < 9) {
     mask = mask & ~(0xFF << (destBit + srcSize));
-    data = dest[destIndex] & ~mask;
-    dest[destIndex] = data | ((src[srcIndex] << destBit) & mask);
+    data = *dest & ~mask;
+    *dest = data | ((*src << destBit) & mask);
     return;
   }
   // first element
-  data = dest[destIndex] & ~mask;
-  dest[destIndex] = data | (src[srcIndex] << destBit);
+  data = *dest & ~mask;
+  *dest = data | (*src << destBit);
   destIndex++;
+  dest++;
   // continue
   while (destIndex != destIndexEnd) {
     if (mask != 0xFF)  // array aligned access, this is not needed
     {
-      data = dest[destIndex] & mask;
-      dest[destIndex] = data | (src[srcIndex] >> (8 - destBit));
+      data = *dest & mask;
+      *dest = data | (*src >> (8 - destBit));
     }
     srcIndex++;
-    data = dest[destIndex] & ~mask;
-    dest[destIndex] = data | (src[srcIndex] << destBit);
+    src++;
+    data = *dest & ~mask;
+    *dest = data | (*src << destBit);
     destIndex++;
+    dest++;
   }
   // last element
 
   if ((destIndexEnd != destSize)) {  // check if the end operation would write over boundary
-    if (mask == 0xFF) srcIndex++;    // if we had aligned access, then we need next srcIndex
+    if (mask == 0xFF) {              // if we had aligned access, then we need next srcIndex
+      srcIndex++;
+      src++;
+    }
     // handle remaining bits
     unsigned int remainderBits = endBit & 7;
     if (remainderBits) {
       mask = 0xFF << (remainderBits);
-      data = dest[destIndex] & mask;
-      dest[destIndex] = data | ((src[srcIndex] >> remainderBits) & ~mask);
+      data = *dest & mask;
+      *dest = data | ((*src >> remainderBits) & ~mask);
     }
   }
 }
