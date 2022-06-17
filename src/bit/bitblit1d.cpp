e09e42b319a2dbd614571fd0429e602a02ea7c9e
Squantor <squantmuts@gmail.com>
2022-06-17 17:04:34 +0200

    WIP removed TODO to switch to a flag instead of magic mask

--- src/bit/bitblit1d.cpp
+++ src/bit/bitblit1d.cpp
@@ -11,22 +11,25 @@
  *
  */
 #include <bitblit.hpp>
+#include <string.h>
 
 namespace util {
 
-// TODO change to zero checks and remove indexes
-// TODO we could make aligned access a flag instead of a special mask?
-// TODO: extract array aligned access, RMW is redundant for it
+// WIP change to zero checks and remove indexes
+// TODO: move array aligned access, a memcpy is quicker and RMW is redundant for it
 // but do not forget the last dangling bits in that case!
 void bitblit1d(uint8_t *dest, size_t destSize, unsigned int destPos, uint8_t *src, unsigned int srcSize) {
   // compute indexes and limits
-  unsigned int srcIndex = 0;
+  bool alignedWrites = (destPos & 7) == 0;
+  unsigned int destOffset = destPos == 0 ? 0 : 1;  // fixup one element if we have aligned writes
+  unsigned int count = srcSize / 8;
   unsigned int destIndex = destPos / 8;
   dest = dest + destPos / 8;
   unsigned int destBit = destPos & 7;
-  unsigned int destOffset = destPos == 0 ? 0 : 1;
+
   unsigned int destIndexEnd = (destPos + srcSize + destOffset) / 8;
   unsigned int endBit = destBit + srcSize;
+  if (count + destIndex > destSize) count = destSize - destIndex - destOffset;
   if (destIndexEnd > destSize) destIndexEnd = destSize;  // limit end index to destination size
   // compute masks
   uint8_t mask = 0xFF << destBit;
@@ -45,23 +48,22 @@ void bitblit1d(uint8_t *dest, size_t destSize, unsigned int destPos, uint8_t *sr
   dest++;
   // continue
   while (destIndex != destIndexEnd) {
-    if (mask != 0xFF)  // array aligned access, this is not needed
+    if (!alignedWrites)  // array aligned access, this is not needed
     {
       data = *dest & mask;
       *dest = data | (*src >> (8 - destBit));
     }
-    srcIndex++;
     src++;
     data = *dest & ~mask;
     *dest = data | (*src << destBit);
     destIndex++;
     dest++;
+    count--;
   }
   // last element
 
   if ((destIndexEnd != destSize)) {  // check if the end operation would write over boundary
-    if (mask == 0xFF) {              // if we had aligned access, then we need next srcIndex
-      srcIndex++;
+    if (alignedWrites) {             // if we had aligned access, then we need next srcIndex
       src++;
     }
     // handle remaining bits
