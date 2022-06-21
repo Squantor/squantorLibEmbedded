5c8d9ea938570aa8aa72f520cae6eee5cd7fa3dc
Squantor <squantmuts@gmail.com>
2022-06-21 20:09:20 +0200

    WIP 2d bitblit

--- inc/bitblit.hpp
+++ inc/bitblit.hpp
@@ -28,13 +28,29 @@ enum class bitblitOperation { OP_MOV, OP_NOT, OP_AND, OP_OR, OP_XOR };
  * @brief One dimensional bit block transfer function
  *
  * @param dest      destination buffer
- * @param destWidth destination buffer in bytes
+ * @param destWidth destination buffer in bytes TODO: Change this to bits, makes more sense
  * @param destX     destination position in bits
  * @param src       source buffer
  * @param srcWidth  sourcebuffer size in bits
  */
 void bitblit1d(__restrict uint8_t *dest, size_t destWidth, unsigned int destX, __restrict uint8_t *src, unsigned int srcWidth,
                bitblitOperation op);
+
+/**
+ * @brief Two dimensional bit block transfer
+ *
+ * @param dest        destination buffer
+ * @param destWidth   destination buffer width
+ * @param destHeight  destination buffer height
+ * @param destX       destination X position to write source
+ * @param destY       destination Y position to write source
+ * @param src         source buffer
+ * @param srcWidth    source width
+ * @param srcHeight   source height
+ * @param op          operation to execute
+ */
+void bitblit2d(__restrict uint8_t *dest, unsigned int destWidth, unsigned int destHeight, unsigned int destX, unsigned int destY,
+               __restrict uint8_t *src, unsigned int srcWidth, unsigned int srcHeight, bitblitOperation op);
 
 /**
  * @brief Read Modify Write while masking the source and displacing it with operation op
---
+++ src/bit/bitblit2d.cpp
@@ -0,0 +1,91 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/**
+ *\file bitblit.cpp
+ *
+ * Contains bit block transfer routines
+ *
+ */
+#include <bitblit.hpp>
+#include <string.h>
+
+namespace util {
+
+void bitblit2d(__restrict uint8_t *dest, unsigned int destWidth, unsigned int destHeight, unsigned int destX, unsigned int destY,
+               __restrict uint8_t *src, unsigned int srcWidth, unsigned int srcHeight, bitblitOperation op) {
+  // TODO check if destX and destY are out of bounds
+  // compute counts and clamp if needed
+  const unsigned int elementBitCnt = 8;  // size of elements in src/dest
+  unsigned int countX, countY;
+  bool abortLastWrite;
+  if ((srcWidth + destX) >= destWidth) {
+    countX = (destWidth - destX) / elementBitCnt;
+    abortLastWrite = true;
+  } else {
+    countX = srcWidth / elementBitCnt;
+    abortLastWrite = false;
+  }
+  if ((srcHeight + destY) >= destHeight) {
+    countY = (destHeight - destY);
+  } else {
+    countY = srcHeight;
+  }
+
+  // extract special case for aligned writes and adjust counts
+  bool alignedWrites = (destX & 7) == 0;
+  if (countX > 0 && !alignedWrites) countX--;
+  dest = dest + (destX / elementBitCnt);
+  // compute masks and bit positions
+  unsigned int destBit = destX & 7;
+  unsigned int endBit = destBit + srcWidth;
+  unsigned int remainderBits = endBit & 7;
+  uint8_t mask;
+  uint8_t *currentLine;
+
+  while (countY > 0) {
+    mask = 0xFF << destBit;  // reset mask computation
+    currentLine = dest;
+    // TODO reset countX every iteration!
+    if (srcWidth < elementBitCnt && endBit < 9) {  // case for less then element bits write within a single element
+      mask = mask & ~(0xFF << (destBit + srcWidth));
+      readModifyWrite(currentLine, src, mask, destBit, op);
+
+    } else if (alignedWrites) {  // case for aligned writes
+      unsigned int i = countX;
+      while (i > 0) {
+        readModifyWrite(currentLine, src, mask, 0, op);
+        currentLine++;
+        src++;
+        i--;
+      }
+      if (remainderBits && !abortLastWrite) {  // handle remainder of bits
+        mask = 0xFF >> (remainderBits);
+        readModifyWrite(currentLine, src, mask, 0, op);
+      }
+
+    } else {  // case for unaligned writes single and multiple
+      // first element start
+      readModifyWrite(currentLine, src, mask, destBit, op);
+      currentLine++;
+      while (countX > 0) {  // do the rest
+        readModifyWrite(currentLine, src, ~mask, -(elementBitCnt - destBit), op);
+        src++;
+        readModifyWrite(currentLine, src, mask, destBit, op);
+        currentLine++;
+        countX--;
+      }
+      if (!abortLastWrite && remainderBits) {  // handle last
+        mask = 0xFF >> (remainderBits);
+        readModifyWrite(currentLine, src, mask, -(elementBitCnt - destBit), op);
+      }
+    }
+    // point source pointer to next line
+    dest = dest + (destWidth / elementBitCnt);
+    countY--;
+  }
+}
+};  // namespace util
