4c5dcac32d37f19995abcf3a16baf781460b5fd1
Squantor <squantmuts@gmail.com>
2022-06-24 13:42:41 +0200

    fixed unaligned access negative offset

--- inc/bit/elementpack.hpp
+++ inc/bit/elementpack.hpp
@@ -32,58 +32,59 @@ namespace util {
  * @param op        operation to execute
  */
 template <typename destType, typename srcType>
-void elementPack(destType *__restrict__ dest, srcType *__restrict__ src, int srcShift, bitblitOperation op) noexcept {
-  int maxDestShift = std::numeric_limits<destType>::digits;
-  int maxSrcShift = std::numeric_limits<srcType>::digits;
+void elementPack(destType *__restrict__ dest, const srcType *__restrict__ src, int srcShift, bitblitOperation op) noexcept {
   // same size
   if constexpr (std::numeric_limits<destType>::digits == std::numeric_limits<srcType>::digits) {
-    destType mask = std::numeric_limits<destType>::max();
+    const int maxDestShift = std::numeric_limits<destType>::digits;
+    destType destMask = std::numeric_limits<destType>::max();
 
     if (srcShift == 0) {
-      readModifyWrite(dest, src, mask, srcShift, op);
+      readModifyWrite(dest, src, destMask, srcShift, op);
     } else {
       if (srcShift > 0) {
-        mask = mask << srcShift;
-        readModifyWrite(dest, src, mask, srcShift, op);
+        destMask = destMask << srcShift;
+        readModifyWrite(dest, src, destMask, srcShift, op);
         src++;
-        readModifyWrite(dest, src, static_cast<destType>(~mask), -(maxDestShift - srcShift), op);
+        readModifyWrite(dest, src, static_cast<destType>(~destMask), -(maxDestShift - srcShift), op);
       } else {
-        mask = mask >> -srcShift;
-        readModifyWrite(dest, src, mask, srcShift, op);
+        destMask = destMask >> -srcShift;
+        readModifyWrite(dest, src, destMask, srcShift, op);
       }
     }
   }
   // dest larger then source
   else if constexpr (std::numeric_limits<destType>::digits > std::numeric_limits<srcType>::digits) {
+    const int maxSrcShift = std::numeric_limits<srcType>::digits;
     int elementCount = std::numeric_limits<destType>::digits / std::numeric_limits<srcType>::digits;
     int shiftpos = std::numeric_limits<destType>::digits - std::numeric_limits<srcType>::digits;
-    destType mask = std::numeric_limits<srcType>::max() << shiftpos;
+    destType destMask = std::numeric_limits<srcType>::max() << shiftpos;
     if (srcShift == 0) {
       while (elementCount > 0) {
-        readModifyWrite(dest, src, mask, shiftpos, op);
-        mask = mask >> std::numeric_limits<srcType>::digits;
+        readModifyWrite(dest, src, destMask, shiftpos, op);
+        destMask = destMask >> std::numeric_limits<srcType>::digits;
         shiftpos -= std::numeric_limits<srcType>::digits;
         elementCount--;
         src++;
       }
     } else {
       if (srcShift > 0) {
-        readModifyWrite(dest, src, static_cast<destType>(mask << srcShift), shiftpos + srcShift, op);
-        mask = mask >> (maxSrcShift - srcShift);
+        readModifyWrite(dest, src, static_cast<destType>(destMask << srcShift), shiftpos + srcShift, op);
+        destMask = destMask >> (maxSrcShift - srcShift);
         shiftpos = shiftpos - (maxSrcShift - srcShift);
+        src++;
         while (elementCount > 0) {
-          readModifyWrite(dest, src, mask, shiftpos, op);
-          mask = mask >> std::numeric_limits<srcType>::digits;
+          readModifyWrite(dest, src, destMask, shiftpos, op);
+          destMask = destMask >> std::numeric_limits<srcType>::digits;
           shiftpos -= std::numeric_limits<srcType>::digits;
           elementCount--;
           src++;
         }
       } else {
-        mask = mask >> -srcShift;
+        destMask = destMask >> -srcShift;
         shiftpos = shiftpos - -srcShift;
         while (elementCount > 0) {
-          readModifyWrite(dest, src, mask, shiftpos, op);
-          mask = mask >> std::numeric_limits<srcType>::digits;
+          readModifyWrite(dest, src, destMask, shiftpos, op);
+          destMask = destMask >> std::numeric_limits<srcType>::digits;
           shiftpos -= std::numeric_limits<srcType>::digits;
           elementCount--;
           src++;
