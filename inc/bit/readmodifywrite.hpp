7ee75b05bc749dea45d8d6e32a5c332790317012
Squantor <squantmuts@gmail.com>
2022-06-24 12:18:25 +0200

    changed names of arguments to better reflect what they are applied on

--- inc/bit/readmodifywrite.hpp
+++ inc/bit/readmodifywrite.hpp
@@ -25,39 +25,39 @@ namespace util {
  * @tparam sourceType source element type
  * @param dest        pointer to destination
  * @param src         pointer to source
- * @param mask        mask to apply to source
- * @param shift       shift factor to apply to source
+ * @param srcMask     mask to apply to source
+ * @param srcShift    shift factor to apply to source
  * @param op          operation to perform
  */
 template <typename destType, typename srcType>
-void readModifyWrite(destType *__restrict__ dest, srcType *__restrict__ src, destType mask, int shift,
+void readModifyWrite(destType *__restrict__ dest, srcType *__restrict__ src, destType srcMask, int srcShift,
                      bitblitOperation op) noexcept {
   static_assert(!std::numeric_limits<destType>::is_signed && !std::numeric_limits<srcType>::is_signed,
                 "readModifyWrite only accepts unsigned types!");
   static_assert(std::numeric_limits<destType>::digits >= std::numeric_limits<srcType>::digits,
                 "source should have equal or less bits then destination!");
   destType dataSrc;
-  if (shift > 0)
-    dataSrc = *src << shift;
-  else if (shift < 0)
-    dataSrc = *src >> -(shift);
+  if (srcShift > 0)
+    dataSrc = *src << srcShift;
+  else if (srcShift < 0)
+    dataSrc = *src >> -(srcShift);
   else
     dataSrc = *src;
   switch (op) {
     case bitblitOperation::OP_AND:
-      *dest = *dest & (dataSrc | ~mask);
+      *dest = *dest & (dataSrc | ~srcMask);
       break;
     case bitblitOperation::OP_MOV:
-      *dest = (*dest & ~mask) | (dataSrc & mask);
+      *dest = (*dest & ~srcMask) | (dataSrc & srcMask);
       break;
     case bitblitOperation::OP_NOT:
-      *dest = (*dest & ~mask) | (~dataSrc & mask);
+      *dest = (*dest & ~srcMask) | (~dataSrc & srcMask);
       break;
     case bitblitOperation::OP_OR:
-      *dest = *dest | (dataSrc & mask);
+      *dest = *dest | (dataSrc & srcMask);
       break;
     case bitblitOperation::OP_XOR:
-      *dest = *dest ^ (dataSrc & mask);
+      *dest = *dest ^ (dataSrc & srcMask);
       break;
   }
 }
