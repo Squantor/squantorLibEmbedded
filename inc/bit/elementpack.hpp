6ad3015c9dc35cfdd33a68edb7da996ac6ad409f
Squantor <squantmuts@gmail.com>
2022-06-24 10:16:23 +0200

    WIP element packing routine

---
+++ inc/bit/elementpack.hpp
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/**
+ *\file elementpack.hpp
+ *
+ * element packing routines
+ *
+ */
+#ifndef ELEMENT_PACK_H
+#define ELEMENT_PACK_H
+
+namespace util {}  // namespace util
+
+#endif
