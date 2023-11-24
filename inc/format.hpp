9fb7a9f93d852d26a574c36f2266d1618219cf3c
Bart Bilos <squantmuts+git@gmail.com>
2023-11-24 11:41:22 +0100

    adding more placeholder stuff

---
+++ inc/format.hpp
@@ -0,0 +1,17 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2023 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/*! \file format.hpp
+ *  \brief definitions and for formatting functions
+ *
+ */
+
+#ifndef FORMAT_HPP
+#define FORMAT_HPP
+
+namespace util {}  // namespace util
+
+#endif
---
+++ src/format/format.cpp
@@ -0,0 +1,16 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2023 Bart Bilos
+ * For conditions of distribution and use, see LICENSE file
+ */
+/**
+ * \file format.cpp
+ *
+ * Formatting functions
+ *
+ */
+
+#include <format.hpp>
+
+namespace util {}
