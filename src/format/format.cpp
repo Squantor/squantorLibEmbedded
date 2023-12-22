5000db8be92235e9508eeb33508c3b6d30cccaca
Bart Bilos <squantmuts+git@gmail.com>
2023-12-22 10:22:42 +0100

    folded together print decimals

--- src/format/format.cpp
+++ src/format/format.cpp
@@ -17,6 +17,14 @@ constexpr char hextable[] = "0123456789ABCDEF";
 
 namespace util {
 
+/**
+ * @brief Generic decimal conversion routine
+ *
+ * @param buffer span to append the decimal number to
+ * @param data number to translate to decimal and append
+ * @param divider division factor to start out with
+ * @return span with available space
+ */
 static std::span<char> appendDecGeneric(std::span<char> buffer, std::uint32_t data, uint32_t divider) {
   bool supressDigits = true;
   std::span result = buffer;
@@ -97,7 +105,7 @@ std::span<char> appendHex(std::span<char> buffer, std::uint32_t data) {
   for (int i = 0; i < 8; i++) {
     std::uint32_t value = 0xF0000000 & data;
     value = value >> 28;
-    result = appendDigit(result, static_cast<uint32_t>(value));
+    result = appendDigit(result, static_cast<std::uint32_t>(value));
     data = data << 4;
   }
   return result;
@@ -108,7 +116,7 @@ std::span<char> appendHex(std::span<char> buffer, std::uint16_t data) {
   for (int i = 0; i < 4; i++) {
     std::uint16_t value = 0xF000 & data;
     value = value >> 12;
-    result = appendDigit(result, static_cast<uint32_t>(value));
+    result = appendDigit(result, static_cast<std::uint32_t>(value));
     data = data << 4;
   }
   return result;
@@ -116,8 +124,8 @@ std::span<char> appendHex(std::span<char> buffer, std::uint16_t data) {
 
 std::span<char> appendHex(std::span<char> buffer, std::uint8_t data) {
   std::span result = buffer;
-  result = appendDigit(result, static_cast<uint32_t>(data >> 4));
-  result = appendDigit(result, static_cast<uint32_t>(data));
+  result = appendDigit(result, static_cast<std::uint32_t>(data >> 4));
+  result = appendDigit(result, static_cast<std::uint32_t>(data));
   return result;
 }
 
@@ -126,110 +134,35 @@ std::span<char> appendDec(std::span<char> buffer, std::uint32_t data) {
 }
 
 std::span<char> appendDec(std::span<char> buffer, std::uint16_t data) {
-  bool supressDigits = true;
-  std::span result = buffer;
-  std::uint32_t num = 10000;
-  std::uint8_t idx;
-  while (num > 0) {
-    idx = data / num;
-    if (idx != 0)
-      supressDigits = false;
-    if (!supressDigits)
-      result = appendDigit(result, idx);
-    data -= idx * num;
-    num = num / 10;
-  }
-  if (supressDigits == true)
-    result = appendDigit(result, idx);
-  return result;
+  return appendDecGeneric(buffer, static_cast<std::uint32_t>(data), 10000);
 }
 
 std::span<char> appendDec(std::span<char> buffer, std::uint8_t data) {
-  bool supressDigits = true;
-  std::span result = buffer;
-  std::uint32_t num = 100;
-  std::uint8_t idx;
-  while (num > 0) {
-    idx = data / num;
-    if (idx != 0)
-      supressDigits = false;
-    if (!supressDigits)
-      result = appendDigit(result, idx);
-    data -= idx * num;
-    num = num / 10;
-  }
-  if (supressDigits == true)
-    result = appendDigit(result, idx);
-  return result;
+  return appendDecGeneric(buffer, static_cast<std::uint32_t>(data), 100);
 }
 
 std::span<char> appendDec(std::span<char> buffer, std::int32_t data) {
-  bool supressDigits = true;
-  std::span result = buffer;
-  std::uint32_t num = 1000000000;
-  std::uint8_t idx;
   if (data < 0) {
-    result = appendChar(result, '-');
+    buffer = appendChar(buffer, '-');
     data = -data;
   }
-  while (num > 0) {
-    idx = data / num;
-    if (idx != 0)
-      supressDigits = false;
-    if (!supressDigits)
-      result = appendDigit(result, idx);
-    data -= idx * num;
-    num = num / 10;
-  }
-  if (supressDigits == true)
-    result = appendDigit(result, idx);
-  return result;
+  return appendDecGeneric(buffer, data, 1000000000);
 }
 
 std::span<char> appendDec(std::span<char> buffer, std::int16_t data) {
-  bool supressDigits = true;
-  std::span result = buffer;
-  std::uint32_t num = 10000;
-  std::uint8_t idx;
   if (data < 0) {
-    result = appendChar(result, '-');
+    buffer = appendChar(buffer, '-');
     data = -data;
   }
-  while (num > 0) {
-    idx = data / num;
-    if (idx != 0)
-      supressDigits = false;
-    if (!supressDigits)
-      result = appendDigit(result, idx);
-    data -= idx * num;
-    num = num / 10;
-  }
-  if (supressDigits == true)
-    result = appendDigit(result, idx);
-  return result;
+  return appendDecGeneric(buffer, static_cast<std::uint32_t>(data), 10000);
 }
 
 std::span<char> appendDec(std::span<char> buffer, std::int8_t data) {
-  bool supressDigits = true;
-  std::span result = buffer;
-  std::uint32_t num = 100;
-  std::uint8_t idx;
   if (data < 0) {
-    result = appendChar(result, '-');
+    buffer = appendChar(buffer, '-');
     data = -data;
   }
-  while (num > 0) {
-    idx = data / num;
-    if (idx != 0)
-      supressDigits = false;
-    if (!supressDigits)
-      result = appendDigit(result, idx);
-    data -= idx * num;
-    num = num / 10;
-  }
-  if (supressDigits == true)
-    result = appendDigit(result, idx);
-  return result;
+  return appendDecGeneric(buffer, static_cast<std::uint32_t>(data), 100);
 }
 
 }  // namespace util
